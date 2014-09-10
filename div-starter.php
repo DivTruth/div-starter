e4a95a25ed3bab68e52792d09c0d3e1ffbf61f6f
Nick Worth <nworth@raprec.com>
2014-09-09 17:37:31 -0700

    restructure for modules, addons. remove global and auto-loading

--- div-starter.php
+++ div-starter.php
@@ -3,7 +3,7 @@
  * Plugin Name: Div Starter Application
  * Plugin URI: http://divblend.com/div-starter
  * Description: This is a site boilerplate to create your own application with custom solutions for your project while extending the Div Library Plugin. 
- * Version: 0.1.0 (alpha)
+ * Version: 0.2.0 (alpha)
  * Author: Div Blend Team
  * Author URI: http://divblend.com/div-blend-contributors/
  * Div Library: 1.0
@@ -20,33 +20,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; # Exit if accessed directly
 }
 
-
 /**
- * Main DSA Class
+ * Main site_application Class
  *
- * @class DSA
+ * @class site_application
  * @version	1.0
  */
-global $app_name;
-$app_name = "div_starter";
+final class site_application {
+	
+	/**
+	 * @var string
+	 */
+	public $directory = 'div-starter'; #TODO: If you change directory name, adjust accordingly
 
-final class div_starter {
+	/**
+	 * @var string
+	 */
+	public $version = '0.2.0';
 
 	/**
 	 * @var string
 	 */
 	public $library;
-
-	/**
-	 * @var string
-	 */
-	public $version = '0.1.0';
 	
-	/**
-	 * @var string
-	 */
-	public $directory = 'div-starter';
-
 	/**
 	 * Path Definitions
 	 * @var 	array
@@ -55,27 +51,19 @@ final class div_starter {
 	public $path = array();
 
 	/**
-	 *  Application Class Name
-	 * @var 	string
-	 * @since   1.0
-	 */
-	public $name;
-
-	/**
-	 * @var Div_Library The single instance of the class
+	 * @var The single instance of the site_application class
 	 * @since 1.0
 	 */
 	protected static $_instance = null;
 
 	/**
-	 * Main Div_Library Instance
-	 *
-	 * Ensures only one instance of Div_Library is loaded or can be loaded.
+	 * Main site_application Instance
+	 * Ensures only one instance of site_application is loaded or can be loaded.
 	 *
 	 * @since 1.0
 	 * @static
-	 * @see DIV()
-	 * @return Div_Library - Main instance
+	 * @see $app
+	 * @return site_application - Main instance
 	 */
 	public static function instance($library = "") {
 		if ( is_null( self::$_instance ) ) {
@@ -115,79 +103,61 @@ final class div_starter {
 	}
 
 	/**
-	 * Starter Constructor.
+	 * Site Application Constructor.
 	 * @access public
-	 * @return Starter
+	 * @return Site Application
 	 */
 	public function __construct($library) {
-		global $app_name;
-		$this->name 		= $app_name;
-		$this->library 		= $library;
+		# Pass instance of Div Library upon construction
+		$this->library = $library;
 
-		// Auto-load classes on demand. This effectively creates a queue of autoload functions, and runs through each of them in the order they are defined.
-		if ( function_exists( "__autoload" ) ) {
-			spl_autoload_register( "__autoload" );
-		}
+		# Define application paths
+		$this->define_paths();
 
-		spl_autoload_register( array( $this, 'autoload' ) );
-
-		// Define constants
-		$this->define_constants();
-
-		// Include required files
+		# Include required files
 		$this->includes();
 
-		// Hooks
-		add_action( 'plugins_loaded', array( $this, 'include_fields' ), 20 );
+		# Install Add-ons
+		$this->setup_add_ons();
+
+		# Install Custom Post Type Modules
+		$this->setup_modules();
+
+		# Hooks
 		add_action( 'widgets_init', array( $this, 'include_widgets' ), 20 ); # After Div Library (10)
-		// add_action( 'init', array( $this, 'init' ), 0 );
+		add_action( 'init', array( $this, 'init' ), 0 );
 		// add_action( 'init', array( 'DS_Shortcodes', 'init' ), 20 );
 		
-		// Register ACF Add-on Fields
-		// add_action( 'acf/register_fields', array( $this, 'register_acf_fields' ) );
-		// add_action( 'acf_settings', array( $this, 'acf_settings' ) );
-		
-		// Setup any theme environment settings
+		# Setup any theme environment settings
 		add_action( 'after_setup_theme', array( $this, 'setup_environment' ) );
 
-		// Loaded action
-		do_action( 'divengine_loaded' );
+		# Loaded action
+		do_action( 'site_application_loaded' );
 	}
 
 	/**
-	 * Auto-load Engine classes on demand to reduce memory consumption.
-	 *
-	 * @param mixed $class
-	 * @return void
+	 * Define application paths
 	 */
-	public function autoload( $class ) {
-		$path  = null;
-		$class = strtolower( $class );
-		$file = 'class-' . str_replace( '_', '-', $class ) . '.php';
-
-		if ( strpos( $class, 'de_shortcode_' ) === 0 ) {
-			$path = $this->plugin_path() . '/includes/shortcodes/';
-		} 
-
-		if ( $path && is_readable( $path . $file ) ) {
-			include_once( $path . $file );
-			return;
-		}
-
-		if ( $path && is_readable( $path . $file ) ) {
-			include_once( $path . $file );
-			return;
-		}
-	}
-
-	/**
-	 * Define DE Constants
-	 */
-	private function define_constants() {
+	private function define_paths() {
 		$this->path['plugin_file'] 			= __FILE__;
 		$this->path['url'] 					= WPMU_PLUGIN_URL.'/'.$this->directory.'/';
 		$this->path['dir'] 					= WPMU_PLUGIN_DIR.'/'.$this->directory.'/';
-		$this->path['template_path']		= $this->path['url'].'templates';
+		$this->path['template_path']		= $this->directory.'/templates/';
+		
+		#add-ons
+		$this->path['addons_dir']	= $this->path['dir'] .'addons/';
+		$this->path['addons_url']	= $this->path['url'].'addons/';
+
+		#modules
+		$this->path['modules_dir']	= $this->path['dir'] .'modules/';
+		$this->path['modules_url']	= $this->path['url'].'modules/';
+		
+		#appearance
+		$this->path['appearance_dir']	= $this->path['dir'] .'appearance/';
+		$this->path['images_dir']		= $this->path['appearance_dir'].'images/';
+			
+			$this->path['appearance_url']	= $this->path['url'].'appearance/';
+			$this->path['images_url']		= $this->path['appearance_url'].'images/';
 		
 		#includes
 		$this->path['includes_dir']		= $this->path['dir'] .'includes/';
@@ -212,104 +182,84 @@ final class div_starter {
 	 * Include required core files used in admin and on the frontend.
 	 */
 	private function includes() {
-		// include_once( 'includes/core-functions.php' );			# Core div functions
-		// include_once( ENGINE_INCLUDES_DIR.'class-shortcodes.php' );			# Shortcodes class
+		foreach( glob($this->path['includes_dir'] . 'class-*.php') as $class_path )
+			require_once( $class_path );
 
 		if ( is_admin() ) {
-			// include_once( 'includes/admin/class-de-admin.php' );
+			# Admin only scripts
+			// include_once( 'includes/admin.php' );
 		}
 
 		if ( defined( 'DOING_AJAX' ) ) {
-			// $this->ajax_includes();
+			# Ajax functions for admin and the front-end
+			// include_once( 'includes/ajax.php' );
 		}
 
 		if ( ! is_admin() || defined( 'DOING_AJAX' ) ) {
-			$this->frontend_includes();
+			# Frontend only Scripts
+			// include_once( 'includes/frontend-scripts.php' );	
 		}
 
 	}
-
-	/**
-	 * Include required ajax files.
-	 */
-	public function ajax_includes() {
-		// include_once( 'includes/class-ds-ajax.php' );				# Ajax functions for admin and the front-end
-	}
-
-	/**
-	 * Include required frontend files.
-	 */
-	public function frontend_includes() {
-		// include_once( 'includes/class-ds-template-loader.php' );		# Template Loader
-		// include_once( 'includes/class-ds-frontend-scripts.php' );	# Frontend Scripts
-	}
 	
 	/**
-	 * Include core fields
+	 * Install Add-Ons
 	 */
-	public function include_fields() {
-		// ACF Fields
-		// include_once( 'includes/fields/cpt-fields.php' );
+	public function setup_add_ons() {
+		foreach( glob($this->path['addons_dir'] . '*/index.php') as $class_path )
+			require_once( $class_path );
+		do_action('div_init_addons');
+	}
+
+	/**
+	 * Install CPT Modules
+	 */
+	public function setup_modules() {
+		foreach( glob($this->path['modules_dir'] . '*/_*.php') as $class_path )
+			require_once( $class_path );
+		do_action('div_init_modules');
 	}
 
 	/**
 	 * Include core widgets
 	 */
 	public function include_widgets() {
-		include_once( $this->path['widgets_dir'].'class-widget-banner.php' );
-		add_action( 'starter_loaded', 'register_DS_Widget_Banner', 1);
-
-		// include_once( $this->path['widgets_dir'].'class-widget-text-image.php' );
-		// add_action( 'starter_loaded', 'register_DS_Widget_Text_Image', 1);
+		foreach( glob($this->path['widgets_dir'] . '*.php') as $class_path )
+			require_once( $class_path );
 	}
 
 	/**
-	 * Init DSA when WordPress Initialises.
+	 * Init site_application when WordPress Initialises.
 	 */
 	public function init() {
 		// Before init action
 		do_action( 'before_starter_init' );
 
 		// Load Class instances
-		// $this->user_agent = new DE_Detection();		# Detection class
 
 		// Init action
-		do_action( 'starter_loaded', $this );
+		do_action( 'site_application_loaded', $this );
 	}
 
 	/**
 	 * Ensure theme and server variable compatibility and setup image sizes..
 	 */
 	public function setup_environment() {
-
-	}
-
-	public function register_acf_fields(){  
-		#Include ACF 4.0+ add-ons
-	    // include_once($this->plugin_path().'/includes/fields/acf-repeater/repeater.php');                                  #FIELD: Repeater
-	    // include_once($this->plugin_path().'/includes/fields/acf-gallery/gallery.php');                                    #FIELD: Gallery
-	}
-	public function acf_settings( $options ){
-	    // Activate ACF 4.0+ add-ons
-	    // $options['activation_codes']['repeater'] 		= '';
-	    // $options['activation_codes']['options_page'] 	= '';
-	    // $options['activation_codes']['gallery'] 			= '';
-	    // $options['activation_codes']['flexible_content'] = '';
-	    return $options;
+		
 	}
 
 }
 
 /**
- * Returns the main instance of DE to prevent the need to use globals.
- * @return STARTER
+ * Returns the main instance of site_application to prevent the need to use globals.
+ * @return init_site_application
  */
-if(class_exists('div_starter')) :
+if(class_exists('site_application')) :
 
-	add_action( 'divlibrary_loaded', 'STARTER', 10, 1);
+	add_action( 'divlibrary_loaded', 'init_site_application'); # load after div library is complete
 
-	function STARTER ($library){
-	    div_starter::instance($library);
+	function init_site_application ($library){
+	    $app = site_application::instance($library);
 	}  
 
 endif;
