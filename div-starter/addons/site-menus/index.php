739762f4898bcc4d0398a75aad8c73ed1e2952a8
Nick Worth <nick@divtruth.com>
2015-10-19 23:52:51 -0700

    setup encapsulation for site menus add on, and add position option for tracking scripts

--- div-starter/addons/site-menus/index.php
+++ div-starter/addons/site-menus/index.php
@@ -2,63 +2,29 @@
 /**
  * Plugin Name: Div: Site Menus Add-on
  * Plugin URI: http://divblend.com/div-starter
- * Description: A Div Library add-on that uses ACF (5.0+) to add site specific option pages, equipped with Site Options Panel, Site Info page, and a generic Homepage Options page
+ * Description: A Div Library add-on that uses ACF (5.0+) to add site specific option pages,
+ * equipped with Site Options Panel, Site Info page, and a generic Homepage Options page
  *
  * @package 	Div Library
  * @category 	Add-On
  */
 
+
 /**
- * Ensure theme and server variable compatibility and setup image sizes..
+ * Must load through action otherwise will create duplicate
+ * site application instances because of multiple PHP request
  */
-add_action( 'after_setup_theme', 'init_site_menus' );
-function init_site_menus() {
-	$app = site_application::instance();
+add_action( 'plugins_loaded', 'load_site_menus' );
 
-	/* ACF Option Pages*/
-	if( function_exists('acf_add_options_page') ) {
-
-		/* Main Menu: Site Options */
-		acf_add_options_page(array(
-			'page_title' 	=> 'Site Options',
-			'menu_title'	=> 'Site Options',
-			'menu_slug' 	=> 'site-options',
-			'capability'	=> 'edit_posts',
-			'icon_url'		=> $app->path['images_url'].'admin-menu-icon.png',
-			'position'		=> '2.5',
-			'redirect'		=> false
-		));
-
-		/* Child Menu: Site Information */
-		acf_add_options_page(array(
-			'page_title' 	=> 'Site Information Details',
-			'menu_title'	=> 'Information',
-			'menu_slug' 	=> 'site-info',
-			'capability'	=> 'edit_posts',
-			'parent_slug'	=> 'site-options',
-			'redirect'		=> false
-		));
-
-		/* Child Menu: Child Options Page */
-		// acf_add_options_page(array(
-		// 	'page_title' 	=> 'More Options',
-		// 	'menu_title'	=> 'More Options',
-		// 	'menu_slug' 	=> 'more-options',
-		// 	'capability'	=> 'edit_posts',
-		// 	'parent_slug'	=> 'site-options',
-		// 	'redirect'		=> false
-		// ));
-
+function load_site_menus(){
+	include_once( dirname(__FILE__).'/site-menu.php' );		# Load Addon Class
+	include_once( dirname(__FILE__).'/acf-site-info.php' );	# Register ACF Fields
+	include_once( dirname(__FILE__).'/acf-site-options.php' );	# Register ACF Fields
+	if(class_exists('Site_Menu')){
+		/* Load Site Menu class */
+		$site_menu = new Site_Menu();
+		add_action( 'setup_theme', array($site_menu, 'init') );
 	}
-	
 }
 
-add_action('wp_footer','enque_ga_script');
-function enque_ga_script(){
-	the_field('tracking_scripts','options');
-}
-
-/* Register ACF Fields */
-include_once( dirname(__FILE__).'/acf-site-options.php' );
-include_once( dirname(__FILE__).'/acf-site-info.php' ); 
 ?>
---
+++ div-starter/addons/site-menus/site-menu.php
@@ -0,0 +1,90 @@
+<?php 
+/**
+ * Add-on Name:   Site Menus
+ * Description:   Custom site menus
+ * Dependencies:  ACF 5.0+, Div Library
+ * @link          http://divblend.com/div-starter/add-ons/social-icons/
+ */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+class Site_Menu {
+
+	/**
+	 * Site Application instance
+	 * @var String
+	 */
+	public $app;
+
+	/**
+	 * Site Menu Constructor.
+	 * @access public
+	 */
+	public function __construct() {
+		/* Set Site Application instance */
+		$this->app = site_application::instance();
+	}
+
+	/**
+	 * Init
+	 * @access public
+	 */
+	public function init(){
+		# Hooks & Filters
+		add_action( 'after_setup_theme', array($this, 'init_site_menus') );
+		if( function_exists('get_field') ) {
+			add_action( get_field('tracking_scripts_position'), array($this, 'enque_ga_script') );
+		}
+	}
+
+	/**
+	 * Initiate all the custom site menus
+	 * @access public
+	 */
+	function init_site_menus() {
+		$this->app = site_application::instance();
+
+		/* ACF Option Pages*/
+		if( function_exists('acf_add_options_page') ) {
+
+			/* Main Menu: Site Options */
+			acf_add_options_page(array(
+				'page_title' 	=> 'Site Options',
+				'menu_title'	=> 'Site Options',
+				'menu_slug' 	=> 'site-options',
+				'capability'	=> 'edit_posts',
+				'icon_url'		=> $this->app->path['images_url'].'admin-menu-icon.png',
+				'position'		=> '2.5',
+				'redirect'		=> true
+			));
+
+			/* Child Menu: Site Information */
+			acf_add_options_page(array(
+				'page_title' 	=> 'Site Information Details',
+				'menu_title'	=> 'Information',
+				'menu_slug' 	=> 'site-info',
+				'capability'	=> 'edit_posts',
+				'parent_slug'	=> 'site-options',
+				'redirect'		=> false
+			));
+
+			/* Child Menu: Child Options Page */
+			// acf_add_options_page(array(
+			// 	'page_title' 	=> 'More Options',
+			// 	'menu_title'	=> 'More Options',
+			// 	'menu_slug' 	=> 'more-options',
+			// 	'capability'	=> 'edit_posts',
+			// 	'parent_slug'	=> 'site-options',
+			// 	'redirect'		=> false
+			// ));
+
+		}
+		
+	}
+
+	function enque_ga_script(){
+		the_field('tracking_scripts','options');
+	}
+
+}
+
+?>
