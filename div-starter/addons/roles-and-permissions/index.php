f2d99efd0f6fe61d7569c162f76538f07d6d71d8
Nick Worth <nick@divtruth.com>
2016-01-01 16:38:21 -0700

    Initial setup for roles and permissions addon

---
+++ div-starter/addons/roles-and-permissions/index.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Addon Name: Roles & Permissions
+ * Plugin URI: http://divblend.com/div-starter/add-ons/roles-and-permissions
+ * Description: A Div Library add-on that uses ACF (5.0+) for custom columns
+ *
+ * @package 	Div Library
+ * @category 	Add-On
+ */
+
+/* Load Starter_Roles Class */
+include_once( dirname(__FILE__).'/class-starter-role.php' ); 
+
+?>
